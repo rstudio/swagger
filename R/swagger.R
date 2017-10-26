d6ba0a00f73c34ed77c25d9525dbb5c8463ebe2b
Javier Luraschi <javierluraschi@hotmail.com>
2017-10-26 11:40:49 -0700

    perform changes requested by CRAN

--- R/swagger.R
+++ R/swagger.R
@@ -4,9 +4,9 @@
 #' dynamically generate beautiful documentation from a Swagger-compliant API.
 #'
 #' The main purpose of this package is to enable package authors to create
-#' APIs that are compatible with \link{https://swagger.io/}
-#' and \link{https://www.openapis.org/}.
+#' APIs that are compatible with \url{https://swagger.io/}
+#' and \url{https://www.openapis.org/}.
 #'
-#' To learn more about Swagger visit: \link{https://swagger.io/swagger-ui/}
+#' To learn more about Swagger visit: \url{https://swagger.io/swagger-ui/}
 #'
 "_PACKAGE"
