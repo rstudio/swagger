09db0728f71f4273797a97c974ca971409931a28
Barret Schloerke <schloerke@gmail.com>
2018-12-18 15:13:43 -0500

    add swagger_spec

--- R/paths.R
+++ R/paths.R
@@ -31,3 +31,25 @@ swagger_path <- function() {
 swagger_index <- function() {
   system.file("dist/index.html", package = "swagger")
 }
+
+
+#' Swagger Index File with OpenAPI Path
+#'
+#' Produces a \code{index.html} file that will attempt to access a provided API path.
+#'
+#' @param apiPath Path to an openAPI specification
+#' @export
+#' @return large string containing the contents of \code{\link{swagger_index}()} with
+#'   the appropriate speicification path changed to the \code{apiPath} value.
+#' @examples
+#' if (interactive()) {
+#'   swagger_spec("https://developer.uspto.gov/ibd-api/swagger.json")
+#' }
+swagger_spec <- function(apiPath = "http://petstore.swagger.io/v2/swagger.json") {
+
+  index_file <- swagger_index()
+  index_txt <- paste0(readLines(index_file), collapse = "\n")
+  index_txt <- sub("(http|https)://petstore.swagger.io/v2/swagger.json", apiPath, index_txt)
+
+  index_txt
+}
