58d29c8d23ba59b8198b826ff6969301f64f0b8d
Michal Kouril <xmkouril@gmail.com>
2020-05-26 11:54:48 -0400

    Pulled swagger ui 3.25.4.

--- scripts/download_swagger_ui.R
+++ scripts/download_swagger_ui.R
@@ -4,7 +4,7 @@ library(magrittr)
 library(devtools)
 
 (function() {
-  swagger_ui_version <- "3.23.5"
+  swagger_ui_version <- "3.25.4"
   to_location <- file.path(
     devtools::as.package(".")$path,
     "inst",
