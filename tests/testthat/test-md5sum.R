88af865baf8f9911e788fcdaea2c65f49bd38832
Barret Schloerke <schloerke@gmail.com>
2020-09-11 16:47:50 -0400

    Do not test shimmed files

--- tests/testthat/test-md5sum.R
+++ tests/testthat/test-md5sum.R
@@ -25,12 +25,17 @@ on.exit({
 }, add = TRUE)
 
 for (f in dir(files_location)) {
+  # do not test shimmed files
+  if (f %in% c("index.html")) {
+    next
+  }
   test_that(paste0(f, " md5 sum in package match swagger version released md5sum"), {
     dld <- download.file(paste0("https://unpkg.com/swagger-ui-dist@", swagger_ui_version, "/", f),
                          file.path(tmpd, f),
                          mode = "wb",
                          quiet = TRUE)
     expect_equivalent(tools::md5sum(file.path(files_location, f)),
-                      tools::md5sum(file.path(tmpd, f)))
+                      tools::md5sum(file.path(tmpd, f)),
+                      info = paste0("file: ", f))
   })
 }
